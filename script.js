1477c366533e34a7638e44563bcb6203df2ea1c2
teiqn <teikoresawa@gmail.com>
2023-02-05 16:03:21 +0900

    added buttons and functions.

---
+++ script.js
@@ -0,0 +1,21 @@
+function addNum(x,y){
+    let ans = x + y
+};
+
+function subNum(x,y){
+    let ans = x - y
+};
+
+function divNum(x,y){
+    let ans = x / y
+};
+
+function mulNum(x,y){
+    let ans = x * y
+};
+
+function operate(){
+};
+
+const numSubmit = document.getElementById("num");
+numSubmit.addEventListener("click",changeNum);
